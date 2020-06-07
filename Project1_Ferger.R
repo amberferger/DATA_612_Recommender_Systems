0a2aadc8710065c44c326cde88b5421fd733eb9e
Amber Ferger <aferger1@gmail.com>
2020-06-07 08:55:59 -0400

    project 1 submission

--- Project1_Ferger.R
+++
@@ -1,8 +0,0 @@
-library(tidyverse)
-
-
-rawData <- data.frame(
-  read.csv('S:\\Corp\\CIS\\BI Support\\Amber_Ferger\\Training\\DS_MS\\DATA_612_Recommender_Systems\\project1_data\\ratings_small.csv'))
-
-
-movieMatrix <- spread(rawData, movieId, rating)
